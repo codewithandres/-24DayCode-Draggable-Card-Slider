220bc0cab45d37b4b97479535c162f951a5dece0
Dize dev <axiozweb2022@gmail.com>
2024-04-27 21:03:13 -0500

    se agrego la funcion de los controles del slider

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 
 const carousel = document.querySelector('.carousel');
+const arrowBtns = document.querySelectorAll('.wrapper i');
+const fristCardWidth = carousel.querySelector('.card').offsetWidth;
+
 let isDragin = false, startX, stratScrollLeft;
+// add event listener for the arrows buttons to scroll the carousel left rigth
+arrowBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+
+        carousel.scrollLeft += btn.id === 'left' ? -fristCardWidth : fristCardWidth;
+    });
+});
 
 const draggin = e => {
     if (!isDragin) return
@@ -8,7 +18,7 @@ const draggin = e => {
     carousel.scrollLeft = stratScrollLeft - (e.pageX - startX);
 };
 
-const dragStart = (e) => {
+const dragStart = e => {
     isDragin = true;
 
     startX = e.pageX;
@@ -24,3 +34,23 @@ const dragStop = () => {
 carousel.addEventListener('mousemove', draggin);
 carousel.addEventListener('mousedown', dragStart);
 document.addEventListener('mouseup', dragStop);
+
+// const body = document.body;
+// const scrollWrap = document.querySelector(".smooth-scroll-wrapper");
+// let height = scrollWrap.getBoundingClientRect().height - 1;
+// const speed = 0.04;
+
+// let offset = 0;
+
+// body.style.height = `${Math.floor(height)}px`;
+
+// const smoothScroll = () => {
+//     offset += (window.pageYOffset - offset) * speed;
+
+//     const scroll = `translateY(-${offset}px) translateZ(0)`;
+//     scrollWrap.style.transform = scroll;
+
+//     requestAnimationFrame(smoothScroll);
+// }
+
+// smoothScroll();
